e9a2e4f5aab7a7a82316ae4be48cdcd3f9e9cd81
hellio-kadmiel <kadmielhellio@gmail.com>
2025-10-06 15:51:53 -0300

    adicionando pastas

---
+++ Contador/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace Contador
+{
+
+
+    class Program
+    {
+
+
+        public static void Main()
+        {
+            
+                    double numero = 12.953;
+                    int contador = 10;
+
+                    for (double contador = 0; contador <= 10; contador++)
+                    {
+                        Console.WriteLine($"{numero} x {contador} = {numero * contador}");
+                    }
+                        
+
+
+            //While verifica se a condicao eh verdadeira
+
+            double numero2 = 12;
+            int contador2 = 1;
+
+
+
+            while (contador2 <= 15)
+            {
+                Console.WriteLine($"{contador2} Execucao: {numero2} x {contador2} = {numero2 * contador2}");
+                contador2++;
+                if (contador2 == 5)
+                {
+                    break;
+                }
+
+
+
+            }
+
+
+            // do While
+            int soma = 0, numero = 0;
+
+
+            do
+            {
+                Console.WriteLine("Digite um numero: (0 para parar)");
+                numero = Convert.ToInt32(Console.ReadLine());
+
+                soma += numero;
+
+                //enquanto o numero for diferente de 0
+            } while (numero != 0);
+
+            Console.WriteLine($"O total de soma dos números é: {soma}");
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
---
+++ MenuInterativo/Program.cs
@@ -0,0 +1,83 @@
+﻿
+
+
+using System;
+
+namespace MenuInterativo
+{
+
+    class Program
+    {
+
+
+        public static void Main()
+        {
+
+
+            string opcao;
+            bool exibirMenu = true;
+            while (true)
+            {
+                Console.WriteLine("Digite sua opcao");
+                Console.WriteLine("1 Cadastrar cliente");
+                Console.WriteLine("2 Buscar cliente");
+                Console.WriteLine("3 Apagar cliente");
+                Console.WriteLine(" 4 Encerrar");
+
+                opcao = Console.ReadLine();
+                switch (opcao)
+                {
+                    case "1":
+                        Console.WriteLine("Cliente cadastrado");
+                        break;
+
+
+                    case "2":
+                        Console.WriteLine("busca por cliente");
+                        break;
+
+
+                    case "3":
+                        Console.WriteLine("Apagar cliente");
+                        break;
+                    case "4":
+                        Console.WriteLine("Encerrar");
+                        exibirMenu = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Opcao invalida");
+                        break;
+                    }
+
+                        Console.WriteLine("Saiu da tela");
+
+            }
+
+
+
+
+        }
+
+  
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
